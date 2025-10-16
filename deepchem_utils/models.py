4440c9cca346e8bc07901efb101a49c68956d984
BernalFA <fabernal1230@gmail.com>
2025-10-16 15:16:21 +0200

    implement model factory

---
+++ deepchem_utils/models.py
@@ -0,0 +1,32 @@
+from typing import Callable, Dict
+
+from deepchem_utils.config import ModelsEnum
+
+
+def make_constructor(cls, default_kwargs):
+    def constructor(**kwargs):
+        merged = {**default_kwargs, **kwargs}
+        return cls(**merged)
+    return constructor
+
+
+class ModelFactory:
+    def __init__(self):
+        self._registry: Dict[str, Callable] = {}
+        self._setup()
+
+    def register(self, name: str, constructor: Callable):
+        self._registry[name] = constructor
+
+    def create(self, name: str, mode: str, **kwargs):
+        if name not in self._registry:
+            raise ValueError(f"Model '{name}' not registered.")
+        # add mode
+        kwargs["mode"] = mode
+        return self._registry[name](**kwargs)
+
+    def _setup(self):
+        for model_enum in ModelsEnum:
+            self._registry[model_enum.name] = make_constructor(
+                model_enum.cls, model_enum.defaults
+            )
